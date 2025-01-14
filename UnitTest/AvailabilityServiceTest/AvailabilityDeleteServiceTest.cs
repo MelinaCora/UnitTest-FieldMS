b7f2f96ff16a30139f8e3099f3939a3c7113d368
MelinaCora <melinaccora97@gmail.com>
2025-01-14 08:18:16 -0300

    AvailabilityDeleteService-DeleteAvailability_Should_Throw_Exception_When_CommandFails()

--- UnitTest/AvailabilityServiceTest/AvailabilityDeleteServiceTest.cs
+++ UnitTest/AvailabilityServiceTest/AvailabilityDeleteServiceTest.cs
@@ -5,6 +5,7 @@ using Domain.Entities;
 using Moq;
 using Xunit;
 using System.Threading.Tasks;
+using Application.Exceptions;
 
 namespace UnitTest.AvailabilityServiceTest
 {
@@ -42,6 +43,38 @@ namespace UnitTest.AvailabilityServiceTest
             mockCommand.Verify(c => c.DeleteAvailability(availability), Times.Once,
                 "El método DeleteAvailability debe llamarse exactamente una vez con la disponibilidad obtenida.");
         }
+            
+        [Fact]
+        public async Task DeleteAvailability_Should_Throw_Exception_When_CommandFails()
+        {
+            // ARRANGE
+            var mockCommand = new Mock<IAvailabilityCommand>();
+            var mockGetServices = new Mock<IAvailabilityGetServices>();
+
+            var availabilityId = 1;
+            var availability = new Availability
+            {
+                AvailabilityID = availabilityId
+            };
+
+            mockGetServices
+                .Setup(s => s.GetAvailabilityById(availabilityId))
+                .ReturnsAsync(availability);
+
+            mockCommand
+                .Setup(c => c.DeleteAvailability(availability))
+                .Throws(new Exception("Error al eliminar la disponibilidad."));
+
+            var service = new AvailabilityDeleteService(mockCommand.Object, mockGetServices.Object);
+
+            // ACT & ASSERT
+            var exception = await Assert.ThrowsAsync<Exception>(() => service.DeleteAvailability(availabilityId));
+            Assert.Equal("Error al eliminar la disponibilidad.", exception.Message);
+
+            mockGetServices.Verify(s => s.GetAvailabilityById(availabilityId), Times.Once);
+            mockCommand.Verify(c => c.DeleteAvailability(availability), Times.Once);
+        }
+
     }
 
 
